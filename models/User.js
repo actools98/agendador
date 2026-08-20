62a269222035fb449c02ed37339094755d9ff9a7
actools98 <actools1998@gmail.com>
2026-08-20 18:25:16 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,12 +5,10 @@ class User {
     const stmt = db.prepare('SELECT * FROM users WHERE username = ?');
     return stmt.get(username);
   }
-
   static create(username, passwordHash) {
     const stmt = db.prepare('INSERT INTO users (username, password_hash) VALUES (?, ?)');
     const info = stmt.run(username, passwordHash);
     return info.lastInsertRowid;
   }
 }
-
 module.exports = User;
